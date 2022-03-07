e8abc8cf4b68f9e02f0615624913ce1eed9da32b
TomomiHori <hr.tmm.23@icloud.com>
2022-03-07 14:12:51 +0900

    Initial Commit

---
+++ BottleMail/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BottleMail
+//
+//  Created by Tomomi Hori on 2022/03/06.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
